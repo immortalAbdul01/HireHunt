aacca670ee72900ff776e4cb56e33de8b2aaf48f
Immortal <ak2061435@gmail.com>
2024-08-04 02:19:51 +0530

    update3

--- backend/index.js
+++ backend/index.js
@@ -44,7 +44,7 @@ app.use(cors({
 
 // Manually add CORS headers
 app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', req.headers.origin);
+  res.header('Access-Control-Allow-Origin','https://hire-hunt-front.vercel.app');
   res.header('Access-Control-Allow-Credentials', 'true');
   res.header('Access-Control-Allow-Methods', 'GET,HEAD,OPTIONS,POST,PUT');
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
