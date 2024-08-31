46669cf3426a12f281fb7a6e646a58cf0035b780
Immortal <ak2061435@gmail.com>
2024-08-31 09:40:31 +0530

    update in cors policy q

--- backend/index.js
+++ backend/index.js
@@ -6,7 +6,6 @@ import connectDB from "./utils/db.js";
 import userRoute from "./routes/user.route.js";
 import companyRoute from "./routes/company.route.js";
 import jobRoute from "./routes/job.route.js";
-
 import applicationRoute from "./routes/application.route.js";
 import crypto from 'crypto';
 
@@ -22,33 +21,21 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
-// Simplified CORS setup
 const allowedOrigins = [
   'http://localhost:5173',
-  'https://hire-hunt.vercel.app/'
+  'https://hire-hunt.vercel.app'
 ];
 
 app.use(cors({
   origin: function (origin, callback) {
-    // Allow requests with no origin (like mobile apps, Postman, etc.)
-    if (!origin) return callback(null, true);
-    if (allowedOrigins.indexOf(origin) === -1) {
-      const msg = 'The CORS policy for this site does not allow access from the specified Origin.';
-      return callback(new Error(msg), false);
+    if (!origin || allowedOrigins.indexOf(origin) !== -1) {
+      callback(null, true);
+    } else {
+      callback(new Error('The CORS policy for this site does not allow access from the specified Origin.'), false);
     }
-    return callback(null, true);
   },
   credentials: true
 }));
-
-// Manually add CORS headers
-app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin','https://hire-hunt.vercel.app/');
-  res.header('Access-Control-Allow-Credentials', 'true');
-  res.header('Access-Control-Allow-Methods', 'GET,HEAD,OPTIONS,POST,PUT');
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
-  next();
-});
 
 const PORT = process.env.PORT || 3000;
 
